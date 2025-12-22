9eeb20c0356795ae8eed51f149dae2ea78cc31dd
Vishal Kumar <mrvishal@Mac.lan>
2025-12-23 02:07:26 +0530

    upto lect 09

---
+++ Frontend/JavaScript/01_Basics/04_datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+// 7 types : String , NUmber , Boolean, null, undefined, Symbol , BigInt 
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail ;
+
+const id = Symbol('123') 
+
+const vis = 12
+const sne = 12
+console.log(vis === sne);
+
+// Symbol
+const id2 = Symbol('123')
+const id3 = Symbol('123')
+console.log(id2 === id3);
+console.log('2' === 2);
+
+// BigInt
+const bigNumber = 121332423475398578539475n
+console.log(typeof bigNumber);
+
+// Array 
+const num = ['1','12','vish']
+console.log(num)
+
+// object 
+let myObj = {
+    name : "vishal",
+    age : 24
+}
+
+// Function
+const
